2eeff3e3c24739a94f6693bbb8f2af3e49cc59fe
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-13 18:52:56 +0500

    Add project files.

---
+++ Utilities.cs
@@ -0,0 +1,47 @@
+﻿using System;
+namespace Task1
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Sorts an array in ascending order using bubble sort.
+        /// </summary>
+        /// <param name="arr">Numbers to sort.</param>
+        public static void Sort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+                for (int j = 0; j < n - i - 1; j++)
+                    if (arr[j] > arr[j + 1])
+                    {
+                        // swap temp and arr[i]
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+        }
+
+        /// <summary>
+        /// Searches for the index of a product in an <paramref name="products"/> 
+        /// based on a <paramref name="predicate"/>.
+        /// </summary>
+        /// <param name="products">Products used for searching.</param>
+        /// <param name="predicate">Product predicate.</param>
+        /// <returns>If match found then returns index of product in <paramref name="products"/>
+        /// otherwise -1.</returns>
+        public static int IndexOf(Product[] products, Predicate<Product> predicate)
+        {
+            for (int i = 0; i < products.Length - 1; i++)
+            {
+                var product = products[i - 1];
+              
+                if (products.Equals(predicate))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
