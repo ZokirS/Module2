a8ef19b7d468c13d1ac24d1afd8747da3e06dd5f
Zokir Sobirjonov <33254311+ZokirS@users.noreply.github.com>
2021-07-28 15:18:41 +0500

    Equals method overrided

--- Product.cs
+++ Product.cs
@@ -12,6 +12,17 @@
 
         public double Price { get; set; }
 
-       
+        public override bool Equals(object obj)
+        {
+            if (obj is null || !(obj is Product))
+            {
+                return false;
+            }
+            return Name.Equals((obj as Product).Name) && Price.Equals((obj as Product).Price);
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
